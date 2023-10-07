f8491240a1eb93e8d71a76b4153be9e2083a5a07
okekevicktur <okekevicktur@gmail.com>
2023-10-07 04:59:50 -0700

    Asset added Successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,12 @@ import { useState } from "react";
 
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1 className="bg-red-400">We Begin Here</h1>
-      <div className="card">
-        <p>This is the entry point for our project</p>
-      </div>
+      <Layout />
     </>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = () => {
+  return <div className="bg-white h-24"></div>;
+};
+
+export default Layout;
