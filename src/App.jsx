38f47265f0a7449705f9e9afd97eaa3e8d540173
Ciscoflash <abrahamjude1999@gmail.com>
2023-10-08 15:38:13 +0100

    Added the Carear Route and did some configurations inside the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Layout from "./components/Layout";
+import Carear from "./pages/Carear";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carear" element={<Carear />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { default as ManImage } from "../assets/images/image-3.jpg";
 
-const Main = ({ image, textOverlay, children, textPosition }) => {
+const Main = ({
+  image,
+  textOverlay,
+  children,
+  textPosition,
+  bgText,
+  route,
+}) => {
   const overlayClasses =
     textPosition === "center" ? "text-center" : "text-left";
   return (
     <div>
       <header className="relative">
-        <img src={image} alt="Hero" className="w-full" />
+        <img src={image} alt="Hero" className="w-full min-h-max md:min-h-fit" />
         <div
           className={`absolute top-10 left-5 w-full h-full flex items-center  ${overlayClasses}`}
         >
           <div className="bg-black opacity-70 p-4 text-white ">
             <h1>{textOverlay} </h1>
+          </div>
+          <div className="w-full">
+            <h1 className="text-6xl flex items-center text-white justify-center">
+              {bgText}
+            </h1>
+            <span className="w-full">{route}</span>
           </div>
         </div>
       </header>
